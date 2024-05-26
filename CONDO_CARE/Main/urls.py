89d7021ce4f1146fc425cd94e600fb043a43196e
Tariqul islam Tazim <tariqulislamtazim99@gmail.com>
2024-05-26 15:17:48 +0800

    update the code

--- CONDO_CARE/Main/urls.py
+++ CONDO_CARE/Main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
+
+    path('main/', views.main, name='main'),
     path('login/', views.log_in, name='login'),
     path('signup/', views.sing_up, name='signup'), 
 ]
